d726f2c853e4ae022447bf8c59505b86f4c28ab2
Mike Avdeev <avdeev2033@icloud.com>
2019-07-24 16:15:43 +0300

    refactor(showHello) for brain-games.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import userName from '../index';
+import showHello from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log('');
-
-console.log(`Hello, ${userName()}!`);
+showHello();

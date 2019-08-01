f396e48105df238ac4fee90925d9592a564eb39c
Mike Avdeev <avdeev2033@icloud.com>
2019-08-02 00:22:19 +0300

    realize new architecture of the app

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
-import runGame from '..';
+import brainEven from '../games/even';
 
-const ruleOfGame = {
-  rule: 'Answer "yes" if number even therwise answer "no"',
-  attempt: 3,
-};
-const getRandom = max => Math.floor(Math.random() * Math.floor(max));
-const isEven = num => num % 2 === 0;
-const generateRule = () => {
-  const questionForUser = getRandom(100);
-  const correctAnswer = isEven(questionForUser) ? 'yes' : 'no';
-  return [questionForUser, correctAnswer];
-};
-
-
-runGame(ruleOfGame, generateRule);
+brainEven();

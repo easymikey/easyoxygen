c5079e2416f18ce9e236e3e90d8a4bd641e6617e
Mike Avdeev <avdeev2033@icloud.com>
2019-08-02 00:19:19 +0300

    transfer function brain-even and brain-calc to finder games

---
+++ src/games/even.js
@@ -0,0 +1,16 @@
+import runGame from '..';
+
+const ruleOfGame = {
+  rule: 'Answer "yes" if number even therwise answer "no"',
+  attempt: 3,
+};
+const getRandom = max => Math.floor(Math.random() * Math.floor(max));
+const isEven = num => num % 2 === 0;
+const generateRule = () => {
+  const questionForUser = getRandom(100);
+  const correctAnswer = isEven(questionForUser) ? 'yes' : 'no';
+  return [questionForUser, correctAnswer];
+};
+
+
+export default () => runGame(ruleOfGame, generateRule);

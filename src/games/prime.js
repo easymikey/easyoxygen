3521cf5f9e751cc13cddf65678f55b7b19b69c28
Mike Avdeev <avdeev2033@icloud.com>
2019-08-05 16:59:13 +0300

    fix isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,10 +4,10 @@ const ruleOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no"
 
 const getRandom = max => Math.floor(Math.random() * Math.floor(max));
 const isPrime = (num) => {
-  if (num === 1 || num === 2) {
-    return true;
+  if (num < 2) {
+    return false;
   }
-  for (let i = 2; i < num; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) { // to cut caclucations up
     if (num % i === 0) {
       return false;
     }

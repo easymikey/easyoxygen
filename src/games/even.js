48cdd89e3c525ab40d3bc6482eece9a16f365a0c
Mike Avdeev <avdeev2033@icloud.com>
2019-08-07 12:59:11 +0300

    style a code

--- src/games/even.js
+++ src/games/even.js
@@ -4,10 +4,10 @@ import getRandom from '../utils';
 const ruleOfGame = 'Answer "yes" if number even therwise answer "no"';
 
 const isEven = num => num % 2 === 0;
-const generateQuestion = () => {
-  const questionForUser = getRandom(0, 100);
-  const correctAnswer = isEven(questionForUser) ? 'yes' : 'no';
-  return [questionForUser, correctAnswer];
+const generateGameData = () => {
+  const question = getRandom(0, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
-export default () => runGame(ruleOfGame, generateQuestion);
+export default () => runGame(ruleOfGame, generateGameData);

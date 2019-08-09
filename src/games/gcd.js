58215da9a7cd82143c6b953dd9e38b5f88cc33dd
Mike Avdeev <avdeev2033@icloud.com>
2019-08-09 12:29:27 +0300

    style a code

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,17 +3,17 @@ import getRandom from '../utils';
 
 const ruleOfGame = 'Find the greatest common divisor of given number.';
 
-const commonDiv = (a, b) => {
+const findGcd = (a, b) => {
   if (!b) {
     return a;
   }
-  return commonDiv(b, a % b);
+  return findGcd(b, a % b);
 };
 const generateGameData = () => {
   const num1 = getRandom(1, 100);
   const num2 = getRandom(2, 100);
   const question = `${num1} ${num2}`;
-  const correctAnswer = commonDiv(num1, num2).toString();
+  const correctAnswer = findGcd(num1, num2).toString();
   return [question, correctAnswer];
 };
 

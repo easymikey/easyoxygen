4e38293d58852cea29efb8290ad34f6c2aa487d3
Mike Avdeev <avdeev2033@icloud.com>
2019-08-13 13:43:50 +0300

    add list of games

---
+++ src/list.js
@@ -0,0 +1,13 @@
+import colors from 'colors';
+import { types } './values';
+
+module.exports = function bar() {
+  console.log('');
+  console.log('       CHOOSE A GAME        ');
+  console.log('----------------------------');
+
+  types.forEach((type) => {
+    console.log('%s %s', colors.magenta.italic(`${type.name}`), colors.reset(`  ${type.number}`));
+  });
+  console.log('----------------------------');
+};

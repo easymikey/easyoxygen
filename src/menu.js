f9dd303c4f7dcd4fa8c9aa0c901648c75aebf3c6
Mike Avdeev <avdeev2033@icloud.com>
2019-08-13 22:15:16 +0300

    remove menu.js

---
+++ src/menu.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import brainCalc from 'bin/brain-calc';
+import brainEven from '../bin/brain-even';
+import brainGcd from '../bin/brain-gcd';
+import brainPrime from '../bin/brain-prime';
+import brainProgression from '../bin/brain-progression';
+

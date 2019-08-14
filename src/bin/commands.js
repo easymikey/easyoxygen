1ba7720c7685471735205d322d3955cfe3df5b96
Mike Avdeev <avdeev2033@icloud.com>
2019-08-14 22:36:22 +0300

    add a few new commands

--- src/bin/commands.js
+++ src/bin/commands.js
@@ -2,13 +2,26 @@
 
 import program from 'commander';
 import list from '../list';
+import menu from '../menu';
 
 program
   .command('list')
   .alias('ls')
-  .description('Game menu list')
+  .description('output the list of games')
   .action(() => {
     list();
+  });
+
+program
+  .version('easyoxegen version 2.4.0')
+  .description('minigames');
+
+program
+  .command('menu')
+  .alias('-m')
+  .description('output the game menu')
+  .action(() => {
+    menu();
   });
 
 program.parse(process.argv);

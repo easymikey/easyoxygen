76667f0de755ab4d6c01d89c018618dfd2bcbb4a
Mike Avdeev <avdeev2033@icloud.com>
2019-08-22 14:01:42 +0300

    refactor a code

--- src/games/even.js
+++ src/games/even.js
@@ -1,13 +1,10 @@
-import runGame from '..';
+import runGame, { getSimpleGameData } from '..';
 import getRandom from '../utils';
 
 const ruleOfGame = 'Answer "yes" if number even therwise answer "no"';
 
 export const isEven = num => num % 2 === 0;
-const generateGameData = () => {
-  const question = getRandom(0, 100);
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-  return [question, correctAnswer];
-};
+
+const generateGameData = () => getSimpleGameData(isEven, getRandom(0, 50));
 
 export default () => runGame(ruleOfGame, generateGameData);
